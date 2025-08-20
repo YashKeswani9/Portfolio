0a5345d9dc22be08129183bbeae1d4f5a9bec88c
Yash Keswani <ykeswani09@gmail.com>
2025-08-19 20:56:59 -0400

    updated config

--- next.config.js
+++ next.config.js
@@ -8,6 +8,9 @@ const nextConfig = {
     deviceSizes: [640, 750, 828, 1080, 1200, 1920],
     imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
   },
+  images: {
+    unoptimized: true, // Disable image optimization for now
+  },
 };
 
 module.exports = nextConfig; 
